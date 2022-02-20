f010d6a9a93eccd50326ea9a77ddd6048a0d4ccf
lasi <ibrahim191002@beng.bu.edu.eg>
2022-02-20 16:37:46 +0530

    Update Stickers.js

--- plugins/Stickers.js
+++ plugins/Stickers.js
@@ -1,17 +1,16 @@
-/* X-Script  */
-
-const XcriptX = require('../events');
+const XTroid = require('../events');
 const {MessageType, Mimetype} = require('@adiwajshing/baileys');
 const fs = require('fs');
 const ffmpeg = require('fluent-ffmpeg');
 const {execFile} = require('child_process');
 const cwebp = require('cwebp-bin');
 const Config = require('../config');
+
 const Language = require('../language');
 const Lang = Language.getString('sticker');
 
 if (Config.WORKTYPE == 'private') {
-    XcriptX.addCommand({pattern: 'sticker$', fromMe: true, deleteCommand: false, }, (async (message, match) => {    
+    XcriptX.addCommand({pattern: 'sticker$', fromMe: true, }, (async (message, match) => {    
 
         if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.NEED_REPLY, MessageType.text);
         var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text);
@@ -26,7 +25,7 @@ if (Config.WORKTYPE == 'private') {
         if (message.reply_message.video === false && message.reply_message.image) {
             ffmpeg(location)
                 .outputOptions(["-y", "-vcodec libwebp"])
-                .videoFilters('scale=2000:2000:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=2000:2000:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
+                .videoFilters('scale=600:600:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=600:600:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
                 .save('st.webp')
                 .on('end', async () => {
                     await message.sendMessage(fs.readFileSync('st.webp'), MessageType.sticker);
@@ -46,7 +45,8 @@ if (Config.WORKTYPE == 'private') {
     }));
 }
 else if (Config.WORKTYPE == 'public') {
-    XcriptX.addCommand({pattern: 'sticker$', fromMe: false, deleteCommand: false,}, (async (message, match) => {    
+
+    XcriptX.addCommand({pattern: 'sticker$', fromMe: false,}, (async (message, match) => {    
 
         if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.NEED_REPLY, MessageType.text);
         var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text);
@@ -61,13 +61,12 @@ else if (Config.WORKTYPE == 'public') {
         if (message.reply_message.video === false && message.reply_message.image) {
             ffmpeg(location)
                 .outputOptions(["-y", "-vcodec libwebp"])
-                .videoFilters('scale=2000:2000:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=2000:2000:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
+                .videoFilters('scale=600:600:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=600:600:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
                 .save('st.webp')
                 .on('end', async () => {
                     await message.sendMessage(fs.readFileSync('st.webp'), MessageType.sticker);
             });
-        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid,contextInfo: { forwardingScore: 2, isForwarded: false }, fromMe: true})
-
+        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
 
         }
 
@@ -78,9 +77,9 @@ else if (Config.WORKTYPE == 'public') {
             .on('end', async () => {
                 await message.sendMessage(fs.readFileSync('sticker.webp'), MessageType.sticker);
             });
-        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid,contextInfo: { forwardingScore: 2, isForwarded: false }, fromMe: true})
+        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
     }));
-XcriptX.addCommand({pattern: 'sticker$', fromMe: true, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {    
+    XcriptX.addCommand({pattern: 'sticker$', fromMe: true, dontAddCMDList: true}, (async (message, match) => {    
 
         if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.NEED_REPLY, MessageType.text);
         var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text);
@@ -95,13 +94,12 @@ XcriptX.addCommand({pattern: 'sticker$', fromMe: true, deleteCommand: false, don
         if (message.reply_message.video === false && message.reply_message.image) {
             ffmpeg(location)
                 .outputOptions(["-y", "-vcodec libwebp"])
-                .videoFilters('scale=2000:2000:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=2000:2000:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
+                .videoFilters('scale=600:600:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=600:600:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
                 .save('st.webp')
                 .on('end', async () => {
                     await message.sendMessage(fs.readFileSync('st.webp'), MessageType.sticker);
             });
-        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid,contextInfo: { forwardingScore: 2, isForwarded: false }, fromMe: true})
-
+        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
 
         }
 
@@ -112,6 +110,6 @@ XcriptX.addCommand({pattern: 'sticker$', fromMe: true, deleteCommand: false, don
             .on('end', async () => {
                 await message.sendMessage(fs.readFileSync('sticker.webp'), MessageType.sticker);
             });
-        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid,contextInfo: { forwardingScore: 2, isForwarded: false }, fromMe: true})
+        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
     }));
 }
